3c2ba94670975e32f3ef8fb2e26f494c9b9f54c7
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-05-03 18:38:35 +0200

    database on MongoDB created

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require("express");
 const cors = require("cors");
 require("colors");
 require("dotenv").config();
+const connectDB = require("./dbinit");
 const app = express();
+connectDB();
 
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ entended: true }));
+// app.use(express.urlencoded({ entended: true }));
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my API");
+  res.send(" Welcome to myAPI");
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`.bold);
 });
